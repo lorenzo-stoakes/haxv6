568fefc0731950122c8f677275c4f49993b823a0
Lorenzo Stoakes <lstoakes@gmail.com>
2014-05-07 08:35:07 +0100

    fprintf really makes a lot more sense.
    If the first parameter is a file descriptor, then it’s fprintf.

--- user.h
+++ user.h
@@ -29,7 +29,7 @@ char* strcpy(char*, char*);
 void *memmove(void*, void*, int);
 char* strchr(const char*, char c);
 int strcmp(const char*, const char*);
-void printf(int, char*, ...);
+void fprintf(int, char*, ...);
 char* gets(char*, int max);
 uint strlen(char*);
 void* memset(void*, int, uint);
