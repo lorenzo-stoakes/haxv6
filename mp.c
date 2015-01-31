b4c84cc50613b7d7a8b163e46dc868d7ac2ee234
Lorenzo Stoakes <lstoakes@gmail.com>
2015-01-31 02:08:33 +0000

    Fix accidentally broken link.

--- mp.c
+++ mp.c
@@ -1,7 +1,7 @@
 /*
  * Multiprocessor support
  * Search memory for MP description structures.
- * http:developer.intel.comdesignpentiumdatashts24201606.pdf
+ * http://developer.intel.com/design/pentium/datashts/24201606.pdf
  */
 
 #include "types.h"
