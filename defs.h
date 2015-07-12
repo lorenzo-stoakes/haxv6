9cd743ac7ad203a36c618bf43c16414d970d3889
Lorenzo Stoakes <lstoakes@gmail.com>
2015-07-12 23:13:52 +0100

    Set arglist to void.

--- defs.h
+++ defs.h
@@ -81,8 +81,8 @@ void microdelay(int);
 /* log.c */
 void initlog(void);
 void log_write(struct buf *);
-void begin_trans();
-void commit_trans();
+void begin_trans(void);
+void commit_trans(void);
 
 /* mp.c */
 extern int ismp;
